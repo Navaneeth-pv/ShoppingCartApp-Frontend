27a8ebebdc5569cc2a693951963ea9b00ab3071c
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-09 00:23:55 +0530

    adminlogin added

---
+++ src/app/adminlogin/adminlogin.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-adminlogin',
+  templateUrl: './adminlogin.component.html',
+  styleUrls: ['./adminlogin.component.css']
+})
+export class AdminloginComponent {
+  username=""
+  password=""
+
+  constructor(private router:Router){}
+
+  readlogin=()=>
+  {
+    let data:any={
+      "username":this.username,
+      "password":this.password
+  }
+    console.log(data)
+
+    if (this.username=="admin" && this.password=="12345") {
+
+      this.router.navigate(['/addproduct'])
+      
+      
+    } else {
+      alert("invalid login")
+      
+    }
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const myRoute:Routes=[
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminloginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
