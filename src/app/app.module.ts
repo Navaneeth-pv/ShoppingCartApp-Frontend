5d08cf0d11c14f8ee21f400be48b0c1165a22853
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-09 00:38:07 +0530

    userlogin added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { UserloginComponent } from './userlogin/userlogin.component';
 
 const myRoute:Routes=[
   
@@ -14,7 +15,8 @@ const myRoute:Routes=[
 @NgModule({
   declarations: [
     AppComponent,
-    AdminloginComponent
+    AdminloginComponent,
+    UserloginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/userlogin/userlogin.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-userlogin',
+  templateUrl: './userlogin.component.html',
+  styleUrls: ['./userlogin.component.css']
+})
+export class UserloginComponent {
+
+  username=""
+  password=""
+
+  constructor(private router:Router){}
+
+  readlogin=()=>
+  {
+    let data:any={
+      "username":this.username,
+      "password":this.password
+  }
+    console.log(data)
+
+    if (this.username=="user" && this.password=="456789") {
+
+      this.router.navigate(['/searchviewproduct'])
+      
+      
+    } else {
+      alert("invalid login")
+      
+    }
+  }
+
+}
